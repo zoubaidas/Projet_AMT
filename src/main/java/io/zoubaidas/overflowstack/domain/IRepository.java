09bd07cb52ad658f7eeeeac1e39472ee5eeb6351
zoubaidas <36661627+zoubaidas@users.noreply.github.com>
2020-10-24 23:57:48 +0200

    Update IRepository.java

--- src/main/java/io/zoubaidas/overflowstack/domain/IRepository.java
+++ src/main/java/io/zoubaidas/overflowstack/domain/IRepository.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 
 public interface IRepository<ENTITY extends IEntity, ID extends Id> {
     void save(ENTITY entity);
+
     void remove(ID id);
+
     Optional<ENTITY> findById(ID id);
+
     Collection<ENTITY> findAll();
 }
