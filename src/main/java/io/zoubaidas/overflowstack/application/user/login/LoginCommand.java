f5b1b1df806f7a03e1dd5a5dcb3911621aefb7af
zoubaidas <36661627+zoubaidas@users.noreply.github.com>
2020-10-25 00:12:57 +0200

    Update LoginCommand.java

--- src/main/java/io/zoubaidas/overflowstack/application/user/login/LoginCommand.java
+++ src/main/java/io/zoubaidas/overflowstack/application/user/login/LoginCommand.java
@@ -1,10 +1,12 @@
 package io.zoubaidas.overflowstack.application.user.login;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Builder
 @Getter
+@EqualsAndHashCode
 public class LoginCommand {
     private String email;
     private String password;
