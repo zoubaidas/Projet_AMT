5464caa6a14641707df251a3cb052e9c6f60b65a
zoubaidas <36661627+zoubaidas@users.noreply.github.com>
2020-10-25 23:32:17 +0100

    Update HomePageServlet.java

--- src/main/java/io/zoubaidas/overflowstack/ui/web/home/HomePageServlet.java
+++ src/main/java/io/zoubaidas/overflowstack/ui/web/home/HomePageServlet.java
@@ -8,15 +8,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HomePageServlet", urlPatterns ="/HomePage")
+@WebServlet(name = "HomePageServlet", urlPatterns = "/home")
 public class HomePageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h3>HomePage</h3>");
+        request.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(request, response);
     }
 }
