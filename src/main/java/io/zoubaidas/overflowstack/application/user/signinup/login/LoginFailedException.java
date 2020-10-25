0939c982a1e6772d5eb48b56517e5ca1844c5bee
zoubaidas <36661627+zoubaidas@users.noreply.github.com>
2020-10-25 23:45:40 +0100

    Create LoginFailedException.java

---
+++ src/main/java/io/zoubaidas/overflowstack/application/user/signinup/login/LoginFailedException.java
@@ -0,0 +1,13 @@
+package io.zoubaidas.overflowstack.application.user.signinup.login;
+
+import io.zoubaidas.overflowstack.application.BusinessException;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+public class LoginFailedException extends BusinessException {
+
+    public LoginFailedException(String message) {
+        super(message);
+    }
+}
