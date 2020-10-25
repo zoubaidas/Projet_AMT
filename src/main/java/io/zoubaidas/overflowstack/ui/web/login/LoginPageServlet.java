737e04c5dbb9d40b16f73354e63ae4f2cf5ca596
zoubaidas <36661627+zoubaidas@users.noreply.github.com>
2020-10-25 23:31:44 +0100

    Update LoginPageServlet.java

--- src/main/java/io/zoubaidas/overflowstack/ui/web/login/LoginPageServlet.java
+++ src/main/java/io/zoubaidas/overflowstack/ui/web/login/LoginPageServlet.java
@@ -6,19 +6,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet(name = "LoginPageServlet", urlPatterns ="/LoginPage")
+@WebServlet(name = "LoginPageServlet", urlPatterns = "/login")
 public class LoginPageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h3>LoginPage</h3>");
-        out.println("<h3>Sign in</h3>");
-        out.println("<h3>Create account</h3>");
+        request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
     }
 }
