1c1bb8938b4d37d445fbe42aa4d06ff452dc7da5
zoubaidas <36661627+zoubaidas@users.noreply.github.com>
2020-10-25 23:41:41 +0100

    Update QuestionsPageServlet.java

--- src/main/java/io/zoubaidas/overflowstack/ui/web/quetions/QuestionsPageServlet.java
+++ src/main/java/io/zoubaidas/overflowstack/ui/web/quetions/QuestionsPageServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "QuestionsPageServlet", urlPatterns = "/QuestionsPage" )
+@WebServlet(name = "QuestionsPageServlet", urlPatterns = "/questions" )
 public class QuestionsPageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
