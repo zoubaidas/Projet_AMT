3940b2a9d160dae4ecb1d8205e1488d1b7e88508
zoubaidas <36661627+zoubaidas@users.noreply.github.com>
2020-10-29 23:38:33 +0100

    Update QuestionId.java

--- src/main/java/io/zoubaidas/overflowstack/domain/question/QuestionId.java
+++ src/main/java/io/zoubaidas/overflowstack/domain/question/QuestionId.java
@@ -1,4 +1,19 @@
 package io.zoubaidas.overflowstack.domain.question;
 
-public class QuestionId {
+import io.zoubaidas.overflowstack.domain.Id;
+
+import java.util.UUID;
+
+public class QuestionId extends Id {
+    public QuestionId(){
+        super();
+    }
+    public QuestionId(String id){
+        super(id);
+    }
+
+    public QuestionId(UUID id){
+        super(id);
+    }
 }
+
