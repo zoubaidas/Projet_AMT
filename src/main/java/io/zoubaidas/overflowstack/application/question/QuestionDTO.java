897fc5aa8c5b5d9998c6836f9d8c24822783bf31
zoubaidas <36661627+zoubaidas@users.noreply.github.com>
2020-10-29 23:39:22 +0100

    Update QuestionDTO.java

--- src/main/java/io/zoubaidas/overflowstack/application/question/QuestionDTO.java
+++ src/main/java/io/zoubaidas/overflowstack/application/question/QuestionDTO.java
@@ -1,4 +1,18 @@
 package io.zoubaidas.overflowstack.application.question;
 
+import io.zoubaidas.overflowstack.domain.question.QuestionId;
+import io.zoubaidas.overflowstack.domain.user.UserId;
+import lombok.Builder;
+import lombok.Value;
+
+import java.time.LocalDateTime;
+
+@Value
+@Builder
 public class QuestionDTO {
+    private QuestionId id;
+    private String title;
+    private String body;
+    private UserId authorId;
+    private LocalDateTime createdOn;
 }
