f4572304e34dd378d774801a72ac7d3ffc4952c2
zoubaidas <36661627+zoubaidas@users.noreply.github.com>
2020-10-29 14:32:46 +0100

    fixed for in memoryrepo

--- src/main/java/io/zoubaidas/overflowstack/application/ServiceRegistry.java
+++ src/main/java/io/zoubaidas/overflowstack/application/ServiceRegistry.java
@@ -3,16 +3,15 @@ package io.zoubaidas.overflowstack.application;
 import io.zoubaidas.overflowstack.application.user.signinup.LoginAndRegisterFacade;
 import io.zoubaidas.overflowstack.infrastructure.persistence.memory.InMemoryUserRepository;
 
-public class ServiceRegistry {
-    private InMemoryUserRepository userRepository;
-    private LoginAndRegisterFacade loginAndRegisterFacade;
 
-    public LoginAndRegisterFacade getLoginAndRegisterFacade() {
+public class ServiceRegistry {
+    private  static InMemoryUserRepository userRepository = new InMemoryUserRepository();;
+    private static LoginAndRegisterFacade loginAndRegisterFacade = new LoginAndRegisterFacade(userRepository);
+
+    public  static LoginAndRegisterFacade getLoginAndRegisterFacade() {
         return loginAndRegisterFacade;
     }
 
     public ServiceRegistry() {
-        userRepository = new InMemoryUserRepository();
-        loginAndRegisterFacade = new LoginAndRegisterFacade(userRepository);
     }
 }
