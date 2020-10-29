2e79f7ea58c62b3e462523b01ec670e57be27fbc
zoubaidas <36661627+zoubaidas@users.noreply.github.com>
2020-10-29 23:40:23 +0100

    Update InMemoryRepository.java

--- src/main/java/io/zoubaidas/overflowstack/infrastructure/persistence/memory/InMemoryRepository.java
+++ src/main/java/io/zoubaidas/overflowstack/infrastructure/persistence/memory/InMemoryRepository.java
@@ -11,16 +11,20 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 public abstract class InMemoryRepository<ENTITY extends IEntity<ENTITY, ID>, ID extends Id> implements IRepository<ENTITY, ID> {
+
     private Map<ID, ENTITY> store = new ConcurrentHashMap<>();
 
+    @Override
     public void save(ENTITY entity) {
         store.put(entity.getId(), entity);
     }
 
+    @Override
     public void remove(ID id) {
         store.remove(id);
     }
 
+    @Override
     public Optional<ENTITY> findById(ID id) {
         ENTITY entity = store.get(id);
         if (entity == null) {
@@ -30,6 +34,7 @@ public abstract class InMemoryRepository<ENTITY extends IEntity<ENTITY, ID>, ID 
         return Optional.of(clonedEntity);
     }
 
+    @Override
     public Collection<ENTITY> findAll() {
         return store.values().stream()
                 .map(entity -> entity.deepClone())
